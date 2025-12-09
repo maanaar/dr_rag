34ad125eeb383aaff265d36eb486aa51727f8ff3
manar mohamed <83480249+maanaar@users.noreply.github.com>
2025-12-09 15:01:33 +0200

    Modify clean_text to split and return first segment
    Update clean_text function to return the first part of the text after splitting by ';'.

--- data_handling/data_preprocess.py
+++ data_handling/data_preprocess.py
@@ -10,7 +10,7 @@ CUSTOM_STOPWORDS = {
 def clean_text(text):
     text = str(text).strip()
     text = re.sub(r"\s+", " ", text)
-    return text
+    return text.split(";")[0].strip()
 
 def normalize_query(text):
     text = clean_text(text)
